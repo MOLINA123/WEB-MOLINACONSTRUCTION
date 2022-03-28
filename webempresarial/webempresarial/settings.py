b73cd3e80bb0720f6e242955a1ba856b4529e150
Mosan <molinajesus1990@gmail.com>
2022-03-28 13:27:18 -0500

    'actualizado'

--- webempresarial/webempresarial/settings.py
+++ webempresarial/webempresarial/settings.py
@@ -1,8 +1,15 @@
 
 import os
+import json
 
+import environ
 
 from pathlib import Path
+from django.core.exceptions import ImproperlyConfigured
+
+
+env = environ.Env()
+environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -12,10 +19,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-=b+_2j5i!qgytzz&-p=qz$g9y$^3eie83iiaxy+!@_ef4@71&p'
+#SECRET_KEY = 'django-insecure-=b+_2j5i!qgytzz&-p=qz$g9y$^3eie83iiaxy+!@_ef4@71&p'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG')
 
 ALLOWED_HOSTS = []
 
@@ -73,15 +81,49 @@ TEMPLATES = [
 WSGI_APPLICATION = 'webempresarial.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/4.0/ref/settings/#databases
+#Database
+#https://docs.djangoproject.com/en/4.0/ref/settings/#databases
+
+
+
+
+
+
+with open(os.path.join(BASE_DIR, '../secrets.json')) as secrets_file:
+    secrets = json.load(secrets_file)
+
+def get_secret(setting, secrets=secrets):
+    """Get secret setting or fail with ImproperlyConfigured"""
+    try:
+        return secrets[setting]
+    except KeyError:
+        raise ImproperlyConfigured("Set the {} setting".format(setting))
+
+
+
+DATABASES = {
+    'default':{
+        'ENGINE':get_secret( 'ENGINE'),
+        'NAME':get_secret('NAME'),
+        'USER':get_secret('USER'),
+        'PASSWORD':get_secret('PASSWORD'),
+        'HOST':get_secret('HOST'),
+        'PORT':get_secret('PORT')
+        
+    }
+}
+"""
+
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': get_secret('NAME'),
         'NAME': BASE_DIR / 'db.sqlite3',
+        
     }
 }
+"""
+
 
 
 # Password validation
@@ -149,7 +191,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 
-
+"""
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 
 EMAIL_HOST =  "smtp.gmail.com"
@@ -157,4 +199,19 @@ EMAIL_HOST_PASSWORD = "Mosan#12345"
 EMAIL_HOST_USER = "molinaconstruction2021@gmail.com"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
+"""
+
+
+EMAIL_SETTINGS_FILE = os.path.join(BASE_DIR, '../email_settings.json')
+
+with open(EMAIL_SETTINGS_FILE) as data_file:
+    email_settings = json.load(data_file)
+
+
+EMAIL_BAKEND= email_settings['EMAIL_BAKEND']
+EMAIL_HOST=email_settings['EMAIL_HOST']
+EMAIL_USE_TLS=email_settings['EMAIL_USE_TLS']
+EMAIL_PORT=email_settings['EMAIL_PORT']
+EMAIL_HOST_USER=email_settings['EMAIL_HOST_USER']
+EMAIL_HOST_PASSWORD=email_settings['EMAIL_HOST_PASSWORD']
 
